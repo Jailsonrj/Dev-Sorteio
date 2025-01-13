324b2b6a06958fa69ef84400f61ec00d2a044d53
jailsonrj <jailsonrj222@gmail.com>
2025-01-13 10:59:13 -0300

    Alteration in JS

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ function generateNumber() {
 const min = Math.ceil(document.querySelector(".input-min").value)
 const max = Math.floor(document.querySelector(".input-max").value)
 
-const result = Math.floor(Math.random() * (max - min) + min)
+if ( min >= max){
+    alert("O valor minimo deve ser MENOR que o valor máximo")
+}
+else {
+    const result = Math.floor(Math.random() * (max - min) + min)
 
 alert(result)
+}
 
 }  
